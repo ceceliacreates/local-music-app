8ec3901f41ddb5532e37e3f51f2b746c636fcfc0
ceceliacreates <40367173+ceceliacreates@users.noreply.github.com>
2020-01-03 01:08:17 -0500

    added html route to generate HTML file based on artist ID passed.

--- app/api/html-routes.js
+++ app/api/html-routes.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const db = require("../../models");
+const fs = require("fs");
 
 module.exports = function(app) {
     app.get("/", function(req,res) {
@@ -15,5 +17,31 @@ module.exports = function(app) {
 
     app.get("/edit", function (req,res) {
         res.sendFile(path.join(__dirname, "../public/edit.html"));
+    })
+
+    app.get("/artist/:id", function(req,res) {
+        db.Artist.findOne({where: {id: req.params.id}}).then(function (artist) {
+            const artistName = artist.name;
+            const artistUrl = artist.url;
+            const artistPhoto = artist.photo;
+            data = `
+            <html>
+            <head>
+            <title>Local Artist App</title>
+            </head>
+            <body>
+            <h3>${artistName}</h3>
+            <img src=${artistPhoto}>
+            <h5>${artistUrl}</h5>
+            </body>
+            </html>`;
+
+            fs.writeFile('./app/data/artist.html', data, (err) => {
+                if (err) throw err;
+                console.log("File saved");
+                res.sendFile(path.join(__dirname, "../data/artist.html"));
+            })
+        })
+
     })
 }
